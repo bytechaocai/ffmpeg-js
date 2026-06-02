a180bc2331b1e5ff094f7fba716e3721f8985a52
bytechaocai <39870481+bytechaocai@users.noreply.github.com>
2026-06-02 22:26:11 +0800

    修复bug：转码失败会导致预览报错，现在转码失败的文件会在总结里显示为转码失败，同时会将转码失败的文件输出到日志

--- preview.js
+++ preview.js
@@ -1,7 +1,7 @@
 const cps = require('node:child_process');
 const fs = require('fs');
 const os = require('os');
-const { log, error } = require('node:console');
+const { log, error, warn } = require('node:console');
 
 if (process.argv.length < 3) {
   error('参数错误');
@@ -12,13 +12,22 @@ const workDir = process.argv[2];
 const previewPath = `${workDir}/preview.html`;
 const compressRatioThreshold = Number.parseFloat(process.argv[3]) || 0.9;
 
+// 转码失败的文件
+const failedFile = [];
 const data = JSON.parse(fs.readFileSync(`${workDir}/data.json`).toString());
 for (const ele of data) {
+  const filePath = `${workDir}/${ele.filename}`;
   log(`开始处理文件【${ele.filename}】`);
-  const cmd = `ffprobe -hide_banner "${workDir}/${ele.filename}" 2>&1`;
+  if (!fs.existsSync(filePath) || fs.statSync(filePath).size === 0) {
+    warn(`XX 文件[${ele.filename}]不存在或转码失败`);
+    ele.skip = true;
+    failedFile.push(ele.filename);
+    continue;
+  }
+  const cmd = `ffprobe -hide_banner "${filePath}" 2>&1`;
   const stdout = cps.execSync(cmd, { cwd: workDir }).toString();
   ele.newBitrate = Number.parseInt(stdout.substring(stdout.indexOf('bitrate: ') + 9, stdout.indexOf(' kb/s')));
-  const stat = fs.statSync(`${workDir}/${ele.filename}`);
+  const stat = fs.statSync(filePath);
   ele.newSize = stat.size;
   log(`文件【${ele.filename}】: bitrate=${ele.newBitrate},newSize=${ele.newSize}`);
   ele.compressRatio = (ele.newSize / ele.size);
@@ -41,20 +50,32 @@ for (const ele of data) {
   fs.appendFileSync(previewPath, `<th>${ele.scale}</th>${os.EOL}`);
   fs.appendFileSync(previewPath, `<th>${ele.duration}</th>${os.EOL}`);
   fs.appendFileSync(previewPath, `<th>${ele.size.toLocaleString()}</th>${os.EOL}`);
-  fs.appendFileSync(previewPath, `<th>${ele.newSize.toLocaleString()}</th>${os.EOL}`);
+  if (!ele.skip) {
+    fs.appendFileSync(previewPath, `<th>${ele.newSize.toLocaleString()}</th>${os.EOL}`);
+  } else {
+    fs.appendFileSync(previewPath, "<th>转码失败</th>\r\n");
+  }
   fs.appendFileSync(previewPath, `<th>${ele.bitrate.toLocaleString()}</th>${os.EOL}`);
-  fs.appendFileSync(previewPath, `<th>${ele.newBitrate.toLocaleString()}</th>${os.EOL}`);
-  fs.appendFileSync(previewPath, `<th>${ele.compressRatio.toLocaleString('zh-cn', {
-    style: 'percent'
-  })}</th>${os.EOL}`);
+  if (!ele.skip) {
+    fs.appendFileSync(previewPath, `<th>${ele.newBitrate.toLocaleString()}</th>${os.EOL}`);
+    fs.appendFileSync(previewPath, `<th>${ele.compressRatio.toLocaleString('zh-cn', {
+      style: 'percent'
+    })}</th>${os.EOL}`);
+  } else {
+    fs.appendFileSync(previewPath, "<th>转码失败</th>\r\n");
+    fs.appendFileSync(previewPath, "<th>转码失败</th>\r\n");
+  }
   fs.appendFileSync(previewPath, `      </tr>${os.EOL}`);
   log(`文件[${ele.filename}]写入完成`);
 }
 fs.appendFileSync(previewPath, '    </tbody>\r\n');
 
 // 合计行
-const sumSize = data.reduce((pv, cv) => pv + cv.size, 0);
-const sumSeconds = data.reduce((pv, { duration }) => {
+const sumSize = data.reduce((pv, cv) => cv.skip ? pv : pv + cv.size, 0);
+const sumSeconds = data.reduce((pv, { duration, skip }) => {
+  if (skip) {
+    return pv;
+  }
   // 换算成秒：hh:mm:ss.sss，yy*3600+mm*60+dd毫秒忽略
   const ts = Number.parseInt(duration.substring(0, 2)) * 3600 +
     Number.parseInt(duration.substring(3, 5)) * 60 +
@@ -67,7 +88,7 @@ const minutes = sumSeconds % 3600;
 const minute = Math.floor(minutes / 60);
 const second = minutes % 60;
 const sumDuration = `${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}:${second.toString().padStart(2, '0')}`;
-const sumNewSize = data.reduce((pv, cv) => pv + cv.newSize, 0);
+const sumNewSize = data.reduce((pv, cv) => cv.skip ? pv : pv + cv.newSize, 0);
 const sumCompressRatio = (sumNewSize / sumSize).toLocaleString('zh-cn', { style: 'percent' });
 fs.appendFileSync(previewPath, '    <tfoot>\r\n');
 fs.appendFileSync(previewPath, `      <tr>${os.EOL}`);
@@ -87,6 +108,8 @@ fs.appendFileSync(previewPath, suffix);
 log('html写入完成');
 log(`以下文件压缩比大于等于${compressRatioThreshold}:`);
 const warnMsg = data.filter(ele => ele.compressRatio >= compressRatioThreshold)
-  .map(e => `${e.filename}: ${e.compressRatio.toLocaleString('zh-cn', {style: 'percent'})}`)
+  .map(e => `${e.filename}: ${e.compressRatio.toLocaleString('zh-cn', { style: 'percent' })}`)
   .join(os.EOL);
 log(warnMsg);
+log('以下文件转码失败:');
+log(failedFile.join(os.EOL));
